f5327946d948e3e8d0b7e01018e2193c8978cb2f
Victor Gallegos <316160456@ciencias.unam.mx>
2022-04-26 12:29:58 -0500

    plantilla inicial

---
+++ src/main/java/com/sistema/olimpiadas/OlimpiadasPrometeoApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.olimpiadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlimpiadasPrometeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlimpiadasPrometeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/olimpiadas/OlimpiadasPrometeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.olimpiadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlimpiadasPrometeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
