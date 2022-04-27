1119a00ba9dfb8a2f015ec0f37d4c820771aa88e
Victor Gallegos <316160456@ciencias.unam.mx>
2022-04-27 11:32:59 -0500

    Creando metodo para VER detalles, pertene al CRUD

--- src/main/java/com/sistema/olimpiadas/controlador/JuezController.java
+++ src/main/java/com/sistema/olimpiadas/controlador/JuezController.java
@@ -1,5 +1,7 @@
 package com.sistema.olimpiadas.controlador;
 
+import java.util.Map;
+
 import com.sistema.olimpiadas.modelo.Juez;
 import com.sistema.olimpiadas.servicio.JuezServicio;
 import com.sistema.olimpiadas.util.paginacion.PageRender;
@@ -11,13 +13,29 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class JuezController {
 
   @Autowired
   private JuezServicio juezServicio;
+
+  @GetMapping("/ver/{id}")
+  public String verDetallesDelJuez(@PathVariable(value = "id") Long id, Map<String, Object> modelo,
+      RedirectAttributes flash) {
+    Juez juez = juezServicio.findOne(id);
+    if (juez == null) {
+      flash.addFlashAttribute("error", "El juez no existe en la base de datos");
+      return "redirect:/listar";
+    }
+
+    modelo.put("juez", juez);
+    modelo.put("titulo", "Detalles del juez " + juez.getNombre());
+    return "ver";
+  }
 
   @GetMapping({ "/", "/listar", "" })
   public String listarJueces(@RequestParam(name = "page", defaultValue = "0") int page, Model modelo) {
