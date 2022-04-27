4306ec878c7805c4d280e2c989d52ae179bff3b3
Victor Gallegos <316160456@ciencias.unam.mx>
2022-04-26 20:39:42 -0500

    Crenado el repositorio JuezRepository

---
+++ src/main/java/com/sistema/olimpiadas/repositorios/JuezRepository.java
@@ -0,0 +1,9 @@
+package com.sistema.olimpiadas.repositorios;
+
+import com.sistema.olimpiadas.modelo.Juez;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JuezRepository extends JpaRepository<Juez, Long> {
+
+}
