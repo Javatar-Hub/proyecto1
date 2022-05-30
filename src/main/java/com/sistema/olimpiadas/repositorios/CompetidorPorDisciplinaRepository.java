e5ee1230a9c17c3411a9fcb65af2cfbd3420b050
Demian <53890358+demian35@users.noreply.github.com>
2022-05-30 15:13:56 -0500

    Agregue las consultas para los competidores

--- src/main/java/com/sistema/olimpiadas/repositorios/CompetidorPorDisciplinaRepository.java
+++ src/main/java/com/sistema/olimpiadas/repositorios/CompetidorPorDisciplinaRepository.java
@@ -3,7 +3,21 @@ package com.sistema.olimpiadas.repositorios;
 import com.sistema.olimpiadas.modelo.CompetidorPorDisciplina;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.*;
 
+@Repository
 public interface CompetidorPorDisciplinaRepository extends JpaRepository<CompetidorPorDisciplina, Long> {
+
+    @Query(value = "SELECT * FROM competidores WHERE competidores.nombre LIKE %q%",nativeQuery = true)
+    List<CompetidorPorDisciplina> findByNombre(@Param("q") String q);
+
+    @Query(value = "SELECT * FROM competidores WHERE competidores.apellido_paterno LIKE %q%",nativeQuery = true)
+    List<CompetidorPorDisciplina> findByApellidopaterno(@Param("q") String q);
+
+    @Query(value = "SELECT * FROM competidores WHERE competidores.apellido_materno LIKE %q%",nativeQuery = true)
+    List<CompetidorPorDisciplina> findByApellidomaterno(@Param("q") String q);
 
 }
