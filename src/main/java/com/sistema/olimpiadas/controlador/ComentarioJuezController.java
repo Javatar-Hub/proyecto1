692fee0c5ba8fe6a5886888376b910cc14fb821c
juanma32 <juanmanuel32@ciencias.unam.mx>
2022-06-06 16:03:25 -0600

    index

--- src/main/java/com/sistema/olimpiadas/controlador/ComentarioJuezController.java
+++ src/main/java/com/sistema/olimpiadas/controlador/ComentarioJuezController.java
@@ -1,11 +1,18 @@
 package com.sistema.olimpiadas.controlador;
 
+import java.util.List;
 import java.util.Map;
 
 import com.sistema.olimpiadas.modelo.ComentarioJuez;
+import com.sistema.olimpiadas.modelo.CompetidorPorDisciplina;
+import com.sistema.olimpiadas.modelo.Disciplina;
 import com.sistema.olimpiadas.servicio.ComentarioJuezServicio;
+import com.sistema.olimpiadas.servicio.CompetidorPorDisciplinaImpl;
+import com.sistema.olimpiadas.servicio.DisciplinaServicio;
+import com.sistema.olimpiadas.servicio.DisciplinaServicioImpl;
 import com.sistema.olimpiadas.util.paginacion.PageRender;
 
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,22 +22,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ComentarioJuezController {
   @Autowired
-  private ComentarioJuezServicio comentarioJuezServicio;
+  private DisciplinaServicio disciplinaServicio;
 
   @GetMapping({ "/listarComentarios" })
-  public String listarJueces(@RequestParam(name = "page", defaultValue = "0") int page, Model modelo) {
+  public String listarComentarios(@RequestParam(name = "page", defaultValue = "0") int page, Model modelo) {
     Pageable pageRequest = PageRequest.of(page, 10);
-    Page<ComentarioJuez> jueces = comentarioJuezServicio.visualizarComentarios(pageRequest);
-    PageRender<ComentarioJuez> pageRender = new PageRender<>("/listar", jueces);
+    Page<Disciplina> disciplinas = disciplinaServicio.visualizarDisciplinas(pageRequest);
+    PageRender<Disciplina> pageRender = new PageRender<>("/listarComentarios", disciplinas);
 
-    modelo.addAttribute("titulo", "Listado de jueces");
-    modelo.addAttribute("jueces", jueces);
+    modelo.addAttribute("titulo", "Listado de Disciplina");
+    modelo.addAttribute("Disciplinas", disciplinas);
     modelo.addAttribute("page", pageRender);
 
     return "listarComentarios";
